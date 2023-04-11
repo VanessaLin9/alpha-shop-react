0f5058f200688dbe2db63ba5d699610f3aac960a
VanessaLin9 <vanessa7591@gmail.com>
2023-04-11 17:30:14 +0800

    2 hash solution

---
+++ src/container/Step1Container.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import shallow from 'zustand/shallow';
+import Step1 from '../components/03_Step1';
+import useCartStore from '../store/useCartStore';
+
+const Step1Container = React.forwardRef();
+
+export default Step1Containerx
